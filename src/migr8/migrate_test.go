1438c92d9834f1ba58eb19d4eac4c20ab6ca4b40
Nicholas Silva <nick@silvamerica.com>
2015-12-29 23:39:10 +0000

    Add Test for TTLs

--- src/migr8/migrate_test.go
+++ src/migr8/migrate_test.go
@@ -43,6 +43,40 @@ func Test_MigrateAllKeysWithAPrefix(t *testing.T) {
 	}
 }
 
+func Test_MigrateAllKeysWithTTLs(t *testing.T) {
+	ClearRedis()
+
+	config = Config{
+		Source:  sourceServer.url,
+		Dest:    destServer.url,
+		Workers: 1,
+		Batch:   10,
+		Prefix:  "bar",
+	}
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("bar:%d", i)
+		sourceServer.conn.Do("SET", key, i, "EX", 600)
+	}
+
+	RunAction(migrateKeys)
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("bar:%d", i)
+		exists, _ := redis.Bool(destServer.conn.Do("EXISTS", key))
+
+		if !exists {
+			t.Errorf("Could not find a key %d that should have been migrated", key)
+		}
+
+		ttl, _ := redis.Int64(destServer.conn.Do("PTTL", key))
+
+		if ttl < 1 || ttl > 600000 {
+			t.Errorf("Could not find a TTL for key %d that should have been migrated", key)
+		}
+	}
+}
+
 func Test_DoesNothingInDryRunModeForMigrate(t *testing.T) {
 	ClearRedis()
 
